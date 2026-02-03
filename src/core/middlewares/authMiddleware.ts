80f183d7e12a4a31ee035fe1533f228f69cdde7b
Salma <sa1963645@gmail.com>
2026-02-04 01:44:40 +0200

    feat: Implement moderator management features and enhance authentication middleware with role-based access control.

--- src/core/middlewares/authMiddleware.ts
+++ src/core/middlewares/authMiddleware.ts
@@ -62,15 +62,15 @@ export const adminMiddleware = asyncHandler(async (req: AuthRequest, res: Respon
     throw new AppError("Only admins can do this!", 403, "AUTH_ADMIN_ONLY");
 });
 
-export const creatorMiddleware = asyncHandler(async (req: AuthRequest, res: Response, next: NextFunction) => {
+export const moderatorMiddleware = asyncHandler(async (req: AuthRequest, res: Response, next: NextFunction) => {
     if (
-        (req.user && req.user.role === "CREATOR") ||
+        (req.user && req.user.role === "MODERATOR") ||
         (req.user && req.user.role === "ADMIN")
     ) {
         next();
         return;
     }
-    throw new AppError("Only creators can do this!", 403, "AUTH_CREATOR_ONLY");
+    throw new AppError("Only moderators can do this!", 403, "AUTH_MODERATOR_ONLY");
 });
 
 export const verifiedMiddleware = asyncHandler(async (req: AuthRequest, res: Response, next: NextFunction) => {
