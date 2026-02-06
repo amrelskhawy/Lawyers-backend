d471597b968ab75ab2c32fec94dca8df26f7e70f
Salma <sa1963645@gmail.com>
2026-02-07 01:42:59 +0200

    feat: Introduce booking module with Swagger documentation, API routing, and updated dependencies.

--- src/core/routes/v1/index.ts
+++ src/core/routes/v1/index.ts
@@ -3,6 +3,7 @@ import authRoutes from "../../../modules/auth/auth.routes.js";
 import adminRoutes from "../../../modules/users/admin.routes.js";
 import moderatorRoutes from "../../../modules/moderators/moderators.routes.js";
 import servicesRoutes from "../../../modules/services/services.routes.js";
+import bookingRoutes from "../../../modules/bookings/bookings.routes.js";
 
 const router = Router();
 
@@ -10,5 +11,6 @@ router.use("/auth", authRoutes);
 router.use("/admin", adminRoutes);
 router.use("/moderators", moderatorRoutes);
 router.use("/services", servicesRoutes);
+router.use("/bookings", bookingRoutes);
 
 export default router;
