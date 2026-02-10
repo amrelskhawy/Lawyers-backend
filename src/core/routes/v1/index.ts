8af77e1698314e4c2ae962b0813470fec21d887b
amrelskhawy <amrelskhawy@gmail.com>
2026-02-10 02:42:39 +0200

    return bookings

--- src/core/routes/v1/index.ts
+++ src/core/routes/v1/index.ts
@@ -3,7 +3,7 @@ import authRoutes from "../../../modules/auth/auth.routes.js";
 import adminRoutes from "../../../modules/users/admin.routes.js";
 import moderatorRoutes from "../../../modules/moderators/moderators.routes.js";
 import servicesRoutes from "../../../modules/services/services.routes.js";
-// import bookingRoutes from "../../../modules/bookings/bookings.routes.js";
+import bookingRoutes from "../../../modules/bookings/bookings.routes.js";
 import holidayRoutes from "../../../modules/holidays/holidays.routes.js";
 
 const router = Router();
@@ -12,7 +12,7 @@ router.use("/auth", authRoutes);
 router.use("/admin", adminRoutes);
 router.use("/moderators", moderatorRoutes);
 router.use("/services", servicesRoutes);
-// router.use("/bookings", bookingRoutes);
+router.use("/bookings", bookingRoutes);
 router.use("/holidays", holidayRoutes);
 
 export default router;
