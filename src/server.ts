fe7fc1ce49b574f4609d9360bebdf0f2c4499c76
amrelskhawy <amrelskhawy@gmail.com>
2026-02-11 22:58:51 +0200

    fix origin problem

--- src/server.ts
+++ src/server.ts
@@ -14,25 +14,9 @@ dotenv.config();
 const port = process.env.PORT || 3000;
 const app = express();
 
-const allowedOrigins = [
-    process.env.CLIENT_URL,
-    "http://localhost:3000",
-    "http://localhost:4200",
-    "http://localhost:5173",
-];
-
 app.use(
     cors({
-        origin: (origin, callback) => {
-            // Allow requests with no origin (like mobile apps or curl)
-            if (!origin) return callback(null, true);
-
-            if (allowedOrigins.indexOf(origin) !== -1) {
-                callback(null, true);
-            } else {
-                callback(new Error("Not allowed by CORS"));
-            }
-        },
+        origin: true,
         credentials: true,
     })
 );
