f52fe0b026ebcea669a0fb9121057430391fc77a
Salma <sa1963645@gmail.com>
2026-02-19 23:56:52 +0200

    feat: Introduce a standardized AppResponse utility and implement service management routes with authentication and validation.

--- src/modules/services/services.routes.ts
+++ src/modules/services/services.routes.ts
@@ -12,8 +12,8 @@ import { CreateServiceSchema, UpdateServiceSchema } from "./services.types.js";
 
 const router = express.Router();
 
-router.get("/:id", protect, moderatorMiddleware, getService);
-router.get("/", protect, moderatorMiddleware, listServices); // get services only when user is logged in ( for dashboard )
+router.get("/:id", getService);
+router.get("/", listServices); // get services only when user is logged in ( for dashboard )
 router.post("/", protect, moderatorMiddleware, validateRequest(CreateServiceSchema), createService);
 router.put("/:id", protect, moderatorMiddleware, validateRequest(UpdateServiceSchema), updateService);
 router.delete("/:id", protect, moderatorMiddleware, deleteService);
