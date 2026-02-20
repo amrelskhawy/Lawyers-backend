26061c9a303c7abee544e47aa9d35b08a64d261a
Salma <sa1963645@gmail.com>
2026-02-20 16:50:52 +0200

    feat: add workday routes for fetching and updating working days with protected patch endpoint.

--- src/modules/workdays/workdays.routes.ts
+++ src/modules/workdays/workdays.routes.ts
@@ -1,6 +1,6 @@
 import express from "express";
 import { getAllWorkingDays, updateWorkingDays } from "./workday.controller.js";
-import { protect, adminMiddleware } from "../../core/middlewares/authMiddleware.js";
+import { protect, adminMiddleware, moderatorMiddleware } from "../../core/middlewares/authMiddleware.js";
 import { validateRequest } from "../../core/middlewares/validateRequest.js";
 import { UpdateWorkingDaySchema } from "./workday.types.js";
 
@@ -10,6 +10,7 @@ router.get("/", getAllWorkingDays);
 router.patch("/",
   protect,
   adminMiddleware,
+  moderatorMiddleware,
   validateRequest(UpdateWorkingDaySchema),
   updateWorkingDays
 );
