1d5fec25d5d0334c91ae8e99970fcce6b3e6bac4
amrelskhawy <amrelskhawy@gmail.com>
2026-03-23 20:01:20 +0200

    fix: Ensure environment variables for GoogleAuth credentials are properly defined

--- src/core/services/google/index.ts
+++ src/core/services/google/index.ts
@@ -3,8 +3,8 @@ import { GoogleAuth } from 'google-auth-library';
 
 export const auth = new GoogleAuth({
     credentials: {
-        client_email: process.env.GOOGLE_CLIENT_EMAIL,
-        private_key: process.env.GOOGLE_PRIVATE_KEY!.replace(/\\n/g, '\n'),
+        client_email: process.env.GOOGLE_CLIENT_EMAIL!,
+        private_key: process.env.GOOGLE_PRIVATE_KEY!,
     },
     clientOptions: {
         subject: 'saadalbogamiksa.com@saadalbogamiksa.com'
