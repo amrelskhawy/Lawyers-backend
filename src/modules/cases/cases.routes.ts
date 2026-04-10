fdb8f5d4a7814c3ed5fca81d41550ae6801e4cad
Salma <sa1963645@gmail.com>
2026-04-11 00:14:22 +0200

    feat: implement cases module routes and swagger documentation

--- src/modules/cases/cases.routes.ts
+++ src/modules/cases/cases.routes.ts
@@ -8,6 +8,7 @@ import {
     deleteCase,
     generateCasePdf,
     sendCaseToClient,
+    sendCaseToWhatsapp
 } from "./cases.controller.js";
 import { protect, moderatorMiddleware } from "../../core/middlewares/authMiddleware.js";
 import { validateCreateCase, validateUpdateCase } from "./cases.validator.js";
@@ -23,5 +24,6 @@ router.delete("/:id", protect, moderatorMiddleware, deleteCase);
 
 router.post("/:id/generate-pdf", protect, moderatorMiddleware, generateCasePdf);
 router.post("/:id/send-to-client", protect, moderatorMiddleware, sendCaseToClient);
+router.post("/:id/send-whatsapp", protect, moderatorMiddleware, sendCaseToWhatsapp);
 
 export default router;
